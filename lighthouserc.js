f121efc006922af474a5193795b8c87d9ddfb51f
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2021-02-06 23:40:15 +0100

    Edit Lighthouse assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,8 +6,10 @@ module.exports = {
     upload: {
       target: 'temporary-public-storage',
     },
-    assert: {
-      preset: 'lighthouse:recommended',
+    assertions: {
+      'categories:performance': ['error', { minScore: 0.9 }],
+      'categories:accessibility': ['error', { minScore: 0.9 }],
+      'categories:seo': ['warn', { minScore: 0.9 }],
     },
   },
 };
