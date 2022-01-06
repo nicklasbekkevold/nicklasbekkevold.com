b3dc4eb37010cfd15d914ae744e18b089a5de650
44287251+nicklasbekkevold@users.noreply.github.com <nicklasbekkevold@gmail.com>
2022-01-06 19:00:56 +0100

    Create dark theme

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,32 @@
+import { useTheme } from 'next-themes';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import styles from 'styles/components/Navbar.module.scss';
 
-export const Navbar: React.FC = () => (
-  <nav>
-    <Link href="/">
-      <a>
-        <h1>Nicklas Bekkevold</h1>
-      </a>
-    </Link>
-    <Link href="/projects">Projects</Link>
-    <Link href="/blog">Blog</Link>
-  </nav>
-);
+export const Navbar: React.FC = () => {
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme, setTheme } = useTheme();
+  console.log(resolvedTheme);
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
+  return (
+    <nav className={styles.navbar}>
+      <Link href="/">
+        <a>
+          <h1>Nicklas Bekkevold</h1>
+        </a>
+      </Link>
+      <Link href="/projects">Projects</Link>
+      <Link href="/blog">Blog</Link>
+      <button
+        aria-label="Toggle Dark Mode"
+        type="button"
+        onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
+      >
+        {resolvedTheme === 'dark' ? 'L' : 'D'}
+      </button>
+    </nav>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Layout } from 'components/Layout';
+import { ThemeProvider } from 'next-themes';
 
 import type { AppProps /*, AppContext */ } from 'next/app';
 
@@ -6,9 +7,11 @@ import 'styles/styles.scss';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider defaultTheme="system">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 };
 
