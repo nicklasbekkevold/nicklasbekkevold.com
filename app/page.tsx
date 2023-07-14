43e67afbb7e95f313a2cf75bfb13df73be5a334e
Nicklas Bekkevold <44287251+nicklasbekkevold@users.noreply.github.com>
2023-07-14 11:42:01 +0200

    Setup html css baseline

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,84 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main>
+      <article className="wrapper flow">
+        <h1>nicklasbekkevold.com</h1>
+        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
+          <strong>Donec</strong> malesuada tellus sed scelerisque sodales.
+          Vestibulum in risus ornare, venenatis ante a, ullamcorper dui. Donec
+          suscipit, elit ac condimentum interdum, diam nibh iaculis odio, et
+          fringilla neque ex a nisl. Nullam lectus felis, eleifend et consequat
+          sed, rhoncus eget nisl. Maecenas consectetur faucibus placerat.
+          Maecenas pellentesque neque posuere nisl tincidunt consequat. Quisque
+          quis ex mauris. Vivamus vulputate ligula eget mattis aliquet. Ut
+          viverra molestie neque ac dapibus. Nam tempor malesuada est, sed
+          feugiat eros gravida eget. Maecenas quis facilisis nunc, quis
+          scelerisque justo. Aliquam pulvinar malesuada purus, imperdiet feugiat
+          dolor placerat vitae. Orci varius natoque penatibus et magnis dis
+          parturient montes, nascetur ridiculus mus.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+        <a href="https://nicklasbekkevold.com">nicklasbekkevold.com</a>
 
-      <div className={styles.center}>
         <Image
-          className={styles.logo}
           src="/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
           priority
         />
-      </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <h2>Duis eleifend efficitur quam ut egestas</h2>
+        <p>
+          In ut scelerisque justo. Etiam fermentum mauris enim, eget elementum
+          magna dapibus non. Nam sit amet nunc vitae orci pellentesque lacinia
+          non at turpis. Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit. Praesent mi augue, dapibus quis consectetur at, dictum sed
+          mauris. Quisque tristique magna velit, sed auctor massa dignissim
+          lobortis. Pellentesque metus diam, elementum sit amet dolor et,
+          eleifend lobortis purus. Ut venenatis euismod rutrum. Donec
+          condimentum quis odio a lacinia. Etiam sem nibh, consequat eu
+          ullamcorper vitae, varius nec turpis.
+        </p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+        <p>
+          Proin euismod augue euismod, congue velit nec, viverra lectus. Cras ut
+          elementum massa. Quisque placerat ipsum suscipit tellus semper varius.
+          Curabitur non mauris eu risus ultricies sodales. Mauris ac tellus ac
+          ex vulputate placerat molestie in tellus. Orci varius natoque
+          penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed
+          ut dolor mattis, suscipit urna ac, vulputate nisi. Fusce ultrices, dui
+          sed dictum facilisis, orci tellus aliquam sem, vel ultricies lacus
+          tortor sit amet urna.
+        </p>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+        <p>
+          Cras sagittis fringilla blandit. Donec eget eleifend diam. Vestibulum
+          non velit quis dolor condimentum viverra. Class aptent taciti sociosqu
+          ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc
+          enim sem, rhoncus in interdum ac, eleifend quis tellus. Nam laoreet
+          pharetra urna, vel bibendum nibh eleifend et. Sed pretium pharetra
+          interdum. Maecenas porttitor et felis ut auctor. Integer a neque
+          tempus, commodo tellus a, sollicitudin odio. Nam pretium turpis non
+          pretium consequat. Suspendisse vitae tellus vel nisi convallis
+          ultricies. Proin ut tristique orci. Fusce cursus leo et eros placerat,
+          non hendrerit lacus malesuada. Phasellus commodo blandit orci, a
+          hendrerit nunc mollis rutrum. Phasellus auctor tortor vitae nisi
+          congue, in finibus magna molestie.
+        </p>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <p>
+          Nulla in lorem porttitor, eleifend leo ac, faucibus libero. Integer
+          iaculis non lacus vel maximus. Curabitur ornare gravida maximus. Nulla
+          gravida fringilla pellentesque. Mauris viverra arcu sed ipsum feugiat,
+          vel elementum sem sagittis. In hendrerit gravida lectus, sed mollis
+          diam. Phasellus placerat mi ante, nec interdum quam pulvinar non.
+          Aenean ut urna bibendum, porta nisl nec, finibus tortor. Etiam in arcu
+          ultrices, consectetur nisl eget, gravida neque.
+        </p>
+      </article>
     </main>
   );
 }
