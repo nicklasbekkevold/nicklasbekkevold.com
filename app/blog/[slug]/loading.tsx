68f7d5509b1545e18cc6bfdc0f61c2791b438739
Nicklas Bekkevold <44287251+nicklasbekkevold@users.noreply.github.com>
2023-07-17 12:03:23 +0200

    Add loading components

---
+++ app/blog/[slug]/loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+  return (
+    <main>
+      <article className="wrapper flow">
+        <h2>Blog post</h2>
+        <p>Loading...</p>
+      </article>
+    </main>
+  );
+}
---
+++ app/blog/loading.tsx
@@ -0,0 +1,24 @@
+export default function Loading() {
+  return (
+    <main>
+      <article className="wrapper flow">
+        <h2>Blog</h2>
+        <p>
+          <strong>Donec</strong> malesuada tellus sed scelerisque sodales.
+          Vestibulum in risus ornare, venenatis ante a, ullamcorper dui. Donec
+          suscipit, elit ac condimentum interdum, diam nibh iaculis odio, et
+          fringilla neque ex a nisl. Nullam lectus felis, eleifend et consequat
+          sed, rhoncus eget nisl. Maecenas consectetur faucibus placerat.
+          Maecenas pellentesque neque posuere nisl tincidunt consequat. Quisque
+          quis ex mauris. Vivamus vulputate ligula eget mattis aliquet. Ut
+          viverra molestie neque ac dapibus. Nam tempor malesuada est, sed
+          feugiat eros gravida eget. Maecenas quis facilisis nunc, quis
+          scelerisque justo. Aliquam pulvinar malesuada purus, imperdiet feugiat
+          dolor placerat vitae. Orci varius natoque penatibus et magnis dis
+          parturient montes, nascetur ridiculus mus.
+        </p>
+        <p>Loading...</p>
+      </article>
+    </main>
+  );
+}
